0f181be016c1d166e0188e0c1cb0fe0cdfdd354a
pooladrag <me@iflashlord.com>
2020-10-02 01:16:54 +0330

    Complete the core / Add cros and body-parser to package.json

--- simulator-api.js
+++ simulator-api.js
@@ -1,31 +1,111 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
-
+// allow cors domain requests
 app.use(cors());
 
-// Configuring body parser middleware
+// configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// define a route with method post to get data from front-end
 app.post('/simulate', (req, res) => {
-    // We will be coding here
+    // get body of request
     const body = req.body;
 
-    // Output body
-    console.log(body);
+    // check if for the user will either change or stick selected in the simulator
+    const isSwitchSelected = body["isSwitchSelected"] || false; // set default value to false
+    const numbersOfRound = body["numbersOfRound"] || 100; // set default value to 100
+
+    // calculate and set values
+    const result = simulationSystem(numbersOfRound, isSwitchSelected);
+
+    // serve result
+    res.send(result);
 });
 
-app.get('/simulate', (req, res) => {
-    // We will be coding here
-    let obj = {
-        hi: "hi"
+
+/**
+ * core simulation repeat round of game base on the argument number
+ *
+ * @param {int} numbersOfRound
+ * @param {boolean} isSwitchSelected
+ * @returns object
+ */
+function simulationSystem(numbersOfRound, isSwitchSelected) {
+    let roundLost, roundWon = 0;
+
+    // repeat based on numbers of the round
+    for (let i = 0; i < numbersOfRound; i++) {
+        roundWon += simulateRound(isSwitchSelected);
     }
-    res.send(obj);
-});
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
+    // calculate lost
+    roundLost = numbersOfRound - roundWon;
+
+    // return object of results
+    return {
+        roundWon,
+        roundLost
+    };
+}
+
+
+/**
+ * simulate a round of the game and check if, with random data, the user wins the game or not
+ *
+ * @param {boolean} isSwitchSelected
+ * @returns boolean
+ */
+function simulateRound(isSwitchSelected) {
+
+    // generate two random number for the car and user-selected gate
+    const hasCar = getRandomNumber(0, 3);
+    const selectedGate = getRandomNumber(0, 3);
+
+    // select the gate with the goat to reveal
+    const disclosedGate = gateDiscloser(hasCar, selectedGate);
+
+    if (isSwitchSelected) {
+        return hasCar === gateDiscloser(disclosedGate, selectedGate);
+    } else {
+        return hasCar === selectedGate;
+    }
+}
+
+
+/**
+ * select the gate number from [0, 1, 3] array that does not exist in arguments
+ *
+ * @param {int} hasCar
+ * @param {int} selectedGate
+ * @returns int
+ */
+function gateDiscloser(hasCar, selectedGate) {
+    return [0, 1, 2].find(currentGate => {
+        if (currentGate !== hasCar && currentGate !== selectedGate) {
+            return true;
+        }
+        return false;
+    });
+}
+
+
+/**
+ * generate a random number between min and max
+ *
+ * @param {int} min
+ * @param int max
+ * @returns int
+ */
+function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+app.listen(port, _ => console.log(`Monty Hall API App listening on port ${port}!`));
